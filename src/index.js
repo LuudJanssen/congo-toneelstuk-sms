2df76abe63b65a74e40e75170310082587a0d06f
s145469 <luud-@live.nl>
2019-05-01 17:38:14 +0200

    Change to live mode and add report link

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const numberChunks = _.chunk(numbers, 50);
 numberChunks.forEach(numberSet => {
   const originator = "KittyFit";
   const body =
-    "Hey KittyFit gebruiker! We zien dat je vrienden hard aan het sporten zijn. Wil je de prestaties van je vrienden gisteravond bekijken? google.com";
+    "Hey KittyFit gebruiker! We zien dat je vrienden hard aan het sporten zijn. Wil je de prestaties van je vrienden gisteravond bekijken? https://bit.ly/2LcVhf5";
   const recipients = numberSet.map(({ number }) => number);
 
   messagebirdInstance.messages.create(
--- src/messagebird.js
+++ src/messagebird.js
@@ -1,4 +1,4 @@
-import { messagebirdApiKey } from "../config/messagebird-test.js";
+import { messagebirdApiKey } from "../config/messagebird-live.js";
 import messagebird from "messagebird";
 
 export const messagebirdInstance = messagebird(messagebirdApiKey);
